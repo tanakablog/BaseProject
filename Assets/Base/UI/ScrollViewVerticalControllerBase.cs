63817f00d7733e88437a22de9acaf80461cf4c16
tanakablog <tanakablog@gmail.com>
2016-10-24 00:41:06 +0900

    スクロール制御変更

--- Assets/Base/UI/ScrollViewVerticalControllerBase.cs
+++ Assets/Base/UI/ScrollViewVerticalControllerBase.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UI.Private;
 using UnityEngine;
 
-public class ScrollViewVerticalControllerBase : MonoBehaviour {
+namespace UI
+{
+    /// <summary>
+    /// 縦スクロールビュー制御
+    /// </summary>
+    /// <typeparam name="T">スクロールビューボード</typeparam>
+    public class ScrollViewVerticalControllerBase<T> : ScrollViewControllerBase<T> where T : ScrollViewBoardControllerBase
+    {
+        public override void Initialize(int num)
+        {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }
+    }
 }
